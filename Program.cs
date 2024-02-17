997a002a7414aba9284a47f530eeae0fac1f9a60
Osvaldo R.Moreno Prieto <osvaldo020213.com>
2024-02-17 17:08:37 -0500

    todo: memory representations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,78 @@
 ﻿using System;
 public static class Program
 {
+    #region  Diseño de clases e interfaces
+    public interface IAsalariado
+    {
+        public void CobrarSalarioTrabajador() => System.Console.WriteLine("Cobrando salario como asalariado");
+    }
+
+    public interface IEstudiante
+    {
+        public void RecibirClase() => System.Console.WriteLine("Recibiendo clase");
+    }
+
+    public interface IProfesor
+    {
+        public void ImpartirClase() => System.Console.WriteLine("Impartiendo clase");
+    }
+
+    public class Persona
+    {
+        public string Nombre;
+        public Persona(string nombre) { Nombre = nombre; }
+    }
+
+    public class Plantilla : Persona
+    {
+        public float Salario;
+        public int HorasClase;
+        public Plantilla(string nombre, float salario, int horasClase) : base(nombre)
+        {
+            Salario = salario;
+            HorasClase = horasClase;
+        }
+    }
+
+    public class Trabajador : Plantilla, IAsalariado
+    {
+        public Trabajador(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
+        public void CobrarSalarioTrabajador() => System.Console.WriteLine("Cobrando salario como trabajador");
+    }
+
+    public class Profesor : Trabajador, IProfesor
+    {
+        public Profesor(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
+        public void ImpartirClase() => System.Console.WriteLine("Impartiendo clase como profesor");
+    }
+
+    public class ProfesorAdiestrado : Profesor, IEstudiante
+    {
+        public ProfesorAdiestrado(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
+        public void RecibirClase() => System.Console.WriteLine("Recibiendo clase como profesor adiestrado");
+
+    }
+
+    public class Estudiante : Plantilla, IEstudiante
+    {
+        public Estudiante(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
+        public void RecibirClase() => System.Console.WriteLine("Recibiendo clase como estudiante");
+    }
+    public class AlumnoAyudante : Estudiante, IProfesor, IAsalariado
+    {
+        public float SalarioProfesor;
+        public int HorasClaseProfesor;
+        public AlumnoAyudante(string nombre, float salario, float salarioProfesor, int horasClase, int horasClaseProfesor)
+        : base(nombre, salario, horasClase)
+        {
+            SalarioProfesor = salarioProfesor;
+            HorasClaseProfesor = horasClaseProfesor;
+        }
+        public void CobrarSalarioTrabajador() => System.Console.WriteLine("Cobrando salario como alumno ayudante");
+        public void ImpartirClase() => System.Console.WriteLine("Impartiendo clase como alumno ayudante");
+    }
+    #endregion
+
     public static void Main(string[] args)
     {
         System.Console.WriteLine("Probemos si las clases y las interfaces están bien definidas");
@@ -15,146 +87,86 @@ public static class Program
         System.Console.WriteLine("Alumno ayudante se comporta como Iprofesor?");
         System.Console.WriteLine(alumnoAyudante is IProfesor); // true 
 
-        Escenario();
+        System.Console.WriteLine();
+        escenarioAmbiguedad();
+        System.Console.WriteLine();
+        escenarioGuardia();
     }
 
-//     /* 
-//     Pongamos el siguiente escenario para ejemplificar las ambigüedades que pueden surgir al usar interfaces 
-//     y herencia.
-//     A la universidad se le asigno un presupuesto para contratar el transporte de los estudiantes becados 
-//     y profesores afectados.
-//     Ambas formas de coger el transporte son diferentes.
-//     */
+    /* 
+    Pongamos el siguiente escenario para ejemplificar las ambigüedades que pueden surgir al usar interfaces 
+    y herencia.
+    A la universidad se le asigno un presupuesto para contratar el transporte de los estudiantes becados 
+    y profesores afectados. Y se tiene que un grupo de personas(Personal Transportado) pueden coger ambos transportes.
+    Ambas formas de coger el transporte son diferentes.
+    */
 
-//     interface ITransporte
-//     {
-//         public void CogerOmnibus();
-//     }
 
-//     interface IBecado : ITransporte
-//     {
-//         public new void CogerOmnibus() => System.Console.WriteLine("Cogiendo omnibus como becado");
-//     }
-
-//     interface IAfectado : ITransporte
-//     {
-//         public new void CogerOmnibus() => System.Console.WriteLine("Cogiendo omnibus como afectado");
-//     }
-
-//     class EstudianteBecado : Estudiante, IBecado
-//     {
-//         public EstudianteBecado(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
-//         public void CogerOmnibus() => System.Console.WriteLine("Cogiendo omnibus como estudiante becado");
-//     }
-
-//     class ProfesorAfectado : Profesor, IAfectado
-//     {
-//         public ProfesorAfectado(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
-//         public void CogerOmnibus() => System.Console.WriteLine("Cogiendo omnibus como profesor afectado");
-//     }
-
-//     public static void Escenario()
-//     {
-//         EstudianteBecado estudianteBecado = new EstudianteBecado("Estudiante Becado", 1000, 10);
-//         ProfesorAfectado profesorAfectado = new ProfesorAfectado("Profesor Afectado", 1000, 10);
-//         System.Console.WriteLine("Estudiante becado se comporta como IAfectado?");
-//         System.Console.WriteLine(estudianteBecado is IAfectado); // false
-//         System.Console.WriteLine("Profesor afectado se comporta como IBecado?");
-//         System.Console.WriteLine(profesorAfectado is IBecado); // true
-//         System.Console.WriteLine("Estudiante becado se comporta como ITransporte?");
-//         System.Console.WriteLine(estudianteBecado is ITransporte); // true
-//         System.Console.WriteLine("Profesor afectado se comporta como ITransporte?");
-//         System.Console.WriteLine(profesorAfectado is ITransporte); // true
-
-//     }
-
-//     /*
-//     Luego de un tiempo, el presupuesto se redujo y con ello el viaje de los omnibus,
-//     por lo que al personal de la universidad, se les dio la oportunidad de
-//     coger ambos tipos de transporte. Sin importar si son estudiantes o profesores.
-//     */
-
-//     class PersonalTransporte : IBecado, IAfectado
-//     {
-//         public void CogerOmnibus() => {};
-// }
-
-// public static void Escenario2()
-// {
-//     PersonalTransporte personalTransporte = new PersonalTransporte();
-//     System.Console.WriteLine(PersonalTransporte.CogerOmnibus());
-
-// }
-
-}
-
-public interface IAsalariado
-{
-    public void CobrarSalarioTrabajador() => System.Console.WriteLine("Cobrando salario como asalariado");
-}
-
-public interface IEstudiante
-{
-    public void RecibirClase() => System.Console.WriteLine("Recibiendo clase");
-}
-
-public interface IProfesor
-{
-    public void ImpartirClase() => System.Console.WriteLine("Impartiendo clase");
-}
-
-public class Persona
-{
-    public string Nombre;
-    public Persona(string nombre) { Nombre = nombre; }
-}
-
-public class Plantilla : Persona
-{
-    public float Salario;
-    public int HorasClase;
-    public Plantilla(string nombre, float salario, int horasClase) : base(nombre)
+    interface TransporteEstudiante
     {
-        Salario = salario;
-        HorasClase = horasClase;
+        public void CogerBus() => System.Console.WriteLine("Cogiendo el bus como estudiante");
     }
-}
 
-public class Trabajador : Plantilla, IAsalariado
-{
-    public Trabajador(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
-    public void CobrarSalarioTrabajador() => System.Console.WriteLine("Cobrando salario como trabajador");
-}
-
-public class Profesor : Trabajador, IProfesor
-{
-    public Profesor(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
-    public void ImpartirClase() => System.Console.WriteLine("Impartiendo clase como profesor");
-}
-
-public class ProfesorAdiestrado : Profesor, IEstudiante
-{
-    public ProfesorAdiestrado(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
-    public void RecibirClase() => System.Console.WriteLine("Recibiendo clase como profesor adiestrado");
-
-}
-
-public class Estudiante : Plantilla, IEstudiante
-{
-    public Estudiante(string nombre, float salario, int horasClase) : base(nombre, salario, horasClase) { }
-    public void RecibirClase() => System.Console.WriteLine("Recibiendo clase como estudiante");
-}
-public class AlumnoAyudante : Estudiante, IProfesor, IAsalariado
-{
-    public float SalarioProfesor;
-    public int HorasClaseProfesor;
-    public AlumnoAyudante(string nombre, float salario, float salarioProfesor, int horasClase, int horasClaseProfesor)
-    : base(nombre, salario, horasClase)
+    interface TransporteProfesor
     {
-        SalarioProfesor = salarioProfesor;
-        HorasClaseProfesor = horasClaseProfesor;
+        public void CogerBus() => System.Console.WriteLine("Cogiendo el bus como profesor");
     }
-    public void CobrarSalarioTrabajador() => System.Console.WriteLine("Cobrando salario como alumno ayudante");
-    public void ImpartirClase() => System.Console.WriteLine("Impartiendo clase como alumno ayudante");
+
+    class PersonalTransportado : TransporteEstudiante, TransporteProfesor
+    {
+        public PersonalTransportado() { }
+
+    }
+
+    public static void escenarioAmbiguedad()
+    {
+        PersonalTransportado personalTransportado = new PersonalTransportado();
+        ((TransporteEstudiante)personalTransportado).CogerBus(); // Cogiendo el bus como estudiante
+        ((TransporteProfesor)personalTransportado).CogerBus(); // Cogiendo el bus como profesor
+        // personalTransportado.CogerBus(); // Error de compilación
+    }
+
+
+    /* 
+    Ejemplo de declaración de interfaces explicitas
+    A veces, una clase puede implementar dos interfaces que tienen un método con el mismo nombre.
+    En este caso, se puede usar la declaración de interfaces explícitas para resolver la ambigüedad. 
+    Por ejemplo, en la universidad se le asignó un presupuesto para contratar seguridad debido a la llegada 
+    de personal extranjero con malas intenciones. 
+    Los custodios son responsables de hacer guardia como parte de su trabajo de seguridad.
+    */
+    public interface ISecurity
+    {
+        public void Guardia() => throw new NotImplementedException();
+    }
+
+    public interface ICustodio
+    {
+        public void Guardia() => throw new NotImplementedException();
+    }
+
+    public class Custodio : ICustodio
+    {
+        public Custodio() { }
+        void ICustodio.Guardia() => System.Console.WriteLine("Haciendo guardia como custodio");
+    }
+
+    public class Seguridad : ICustodio, ISecurity
+    {
+        public Seguridad() { }
+        void ICustodio.Guardia() => System.Console.WriteLine("Haciendo guardia como seguridad en el puesto de custodio");
+        void ISecurity.Guardia() => System.Console.WriteLine("Haciendo guardia como seguridad");
+    }
+
+    public static void escenarioGuardia()
+    {
+        Custodio custodio = new Custodio();
+        // custodio.Guardia(); // Error de compilación
+        ((ICustodio)custodio).Guardia(); // Haciendo guardia como custodio
+
+        Seguridad seguridad = new Seguridad();
+        ((ICustodio)seguridad).Guardia(); // Haciendo guardia como seguridad en el puesto de custodio
+        ((ISecurity)seguridad).Guardia(); // Haciendo guardia como seguridad
+    }
 }
 
--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ed81481f43b87a26939135c86e451b2568b894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c0bff8fad5ad1374434495c1bb8761a8e25263")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
