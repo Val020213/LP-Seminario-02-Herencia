65ed81481f43b87a26939135c86e451b2568b894
Osvaldo R.Moreno Prieto <osvaldo020213.com>
2024-02-17 13:37:22 -0500

    git init

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
+
+public interface IAsalariado
+{
+    public void CobrarSalario() => throw new NotImplementedException();
+}
+
+public interface IEstudiante
+{
+    public void RecibirClase() => throw new NotImplementedException();
+}
+
+public interface IProfesor
+{
+    public void ImpartirClase() => throw new NotImplementedException();
+}
+
+public class Persona
+{
+    public string Nombre;
+}
+
+public class Plantilla : Persona
+{
+    public float Salario;
+    public int HorasClase;
+}
+
+public class Trabajador : Plantilla, IAsalariado { }
+
+public class Profesor : Trabajador, IProfesor { }
+
+public class ProfesorAdiestrado : Profesor, IEstudiante { }
+
+public class Estudiante : Plantilla, IEstudiante
+{
+    public void CobrarSalarioEstudiante() => throw new NotImplementedException();
+}
+public class AlumnoAyudante : Estudiante, IProfesor, IAsalariado
+{
+    public float SalarioProfesor;
+}
+
