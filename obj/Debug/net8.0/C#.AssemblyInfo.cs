56a2a84b755beed03462262a7f82c0b6f65a1cc3
Osvaldo R.Moreno Prieto <osvaldo020213.com>
2024-02-18 00:38:24 -0500

    testing

--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c0bff8fad5ad1374434495c1bb8761a8e25263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+997a002a7414aba9284a47f530eeae0fac1f9a60")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
