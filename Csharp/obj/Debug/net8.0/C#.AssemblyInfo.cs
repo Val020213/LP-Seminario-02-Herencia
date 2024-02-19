ecb31c22b2105f1226e4e9b17363bd7b8752243c
DanielMPMatCom <daniel.machado.0206@gmail.com>
2024-02-19 00:08:05 -0500

    details

--- Csharp/obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ Csharp/obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a2a84b755beed03462262a7f82c0b6f65a1cc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
