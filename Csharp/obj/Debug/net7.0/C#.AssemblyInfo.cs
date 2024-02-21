73b437ed220bd36e18cc1814b9e0b2c83d73aec1
DanielMPMatCom <daniel.machado.0206@gmail.com>
2024-02-20 20:24:59 -0500

    python bug fixed

--- Csharp/obj/Debug/net7.0/C#.AssemblyInfo.cs
+++ Csharp/obj/Debug/net7.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08da6d715bed38b6de2af39ac2dd69809e24a77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
